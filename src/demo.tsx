407f8ca21062f105df20bf6bd775d3bbd2f3b0be
lilboox <somdip@gmail.com>
2022-05-15 14:18:13 -0400

    add letters chooser

--- src/demo.tsx
+++ src/demo.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
 import { deepOrange } from "@mui/material/colors";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 const quote = [
   [" ", 10],
@@ -10,14 +12,24 @@ const quote = [
   ["C", 0]
 ];
 
-// Array.from("01") // use to split string into list
+const remaining_letters = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
 export default function LetterAvatars() {
   return (
-    <Stack direction="row" spacing={2}>
-      {quote.map((x) => (
-        <Avatar sx={{ bgcolor: deepOrange[x[1]] }}>{x[0]}</Avatar>
-      ))}
+    <Stack spacing={2}>
+      <Stack direction="row" spacing={2}>
+        {quote.map((x) => (
+          <Avatar sx={{ bgcolor: deepOrange[x[1]] }}>{x[0]}</Avatar>
+        ))}
+      </Stack>
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group"
+      >
+        {remaining_letters.map((x) => (
+          <Button>{x}</Button>
+        ))}
+      </ButtonGroup>
     </Stack>
   );
 }
