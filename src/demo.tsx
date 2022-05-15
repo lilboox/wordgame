5a942e7181dc03959efd44ef678e4dfb1e4f1441
lilboox <somdip@gmail.com>
2022-05-15 16:43:36 -0400

    added callback

--- src/demo.tsx
+++ src/demo.tsx
@@ -5,7 +5,7 @@ import { deepOrange } from "@mui/material/colors";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 
-const quote = [
+const sample_quote = [
   [" ", 10],
   ["A", 500],
   ["B", 500],
@@ -14,22 +14,45 @@ const quote = [
 
 const remaining_letters = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
-export default function LetterAvatars() {
-  return (
-    <Stack spacing={2}>
+class QuoteDisplay extends React.Component {
+  props: {
+    quote?: Array<any>;
+  };
+
+  render() {
+    return (
       <Stack direction="row" spacing={2}>
-        {quote.map((x) => (
+        {this.props.quote.map((x) => (
           <Avatar sx={{ bgcolor: deepOrange[x[1]] }}>{x[0]}</Avatar>
         ))}
       </Stack>
-      <ButtonGroup
-        variant="contained"
-        aria-label="outlined primary button group"
-      >
+    );
+  }
+}
+
+class LetterChoices extends React.Component {
+  letterClick = (text: string) => {
+    console.log(text);
+  };
+
+  render() {
+    return (
+      <ButtonGroup variant="contained">
         {remaining_letters.map((x) => (
-          <Button>{x}</Button>
+          <Button onClick={(e) => this.letterClick(x)} name={x}>
+            {x}
+          </Button>
         ))}
       </ButtonGroup>
+    );
+  }
+}
+
+export default function LetterAvatars() {
+  return (
+    <Stack spacing={2}>
+      <QuoteDisplay quote={sample_quote} />
+      <LetterChoices />
     </Stack>
   );
 }
