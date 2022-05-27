50b9409de274444b8484e4443339b21f7bd63cf5
lilboox <somdip@gmail.com>
2022-05-27 12:06:29 -0400

    rename filenames to be same as components

--- src/WordGame.tsx
+++ src/WordGame.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Stack from "@mui/material/Stack";
-import LetterPicker from "./letterpicker";
-import QuoteDisplay from "./quotedisplay";
+import LetterPicker from "./LetterPicker";
+import QuoteDisplay from "./QuoteDisplay";
 import Button from "@mui/material/Button";
 
 const all_quotes = [
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import ReactDOM from "react-dom/client";
 import { StyledEngineProvider } from "@mui/material/styles";
-import WordGame from "./demo";
+import WordGame from "./WordGame";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <StyledEngineProvider injectFirst>
